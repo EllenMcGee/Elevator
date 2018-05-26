3e46c77af1f6f106a71c60eb4387dc04e18f682f
EllenMcGee <emcgee19@gmail.com>
2018-05-26 10:43:19 -0400

    Constants and Edits

--- src/Elevator.groovy
+++ src/Elevator.groovy
@@ -1,9 +1,10 @@
 class Elevator {
 
+    final Double TRAVEL_TIME_PER_FLOOR = 2.765
+    final Double STOP_TIME_PER_FLOOR = 12
     boolean currentDirection
     Integer currentFloor
-   // Integer numberOfStops
-    def stop // = new Integer[0] //UN HARD CODE
+    def stop
 
     Elevator(int numOfStops){
         stop = new Integer[numOfStops]
--- src/User.groovy
+++ src/User.groovy
@@ -1,9 +1,9 @@
 class User {
 
+    final Double WALK_TIME_PER_FLOOR = 3.137
     Integer currentFloor
     Integer desiredFloor
     Boolean desiredDirection
-
 }
 
 
